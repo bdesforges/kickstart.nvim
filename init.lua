533d7d9a6b4611c0992bfb8c686307325e1a9ed8
bdesforges <bdesforges@gmail.com>
2026-04-25 07:42:49 -0600

    Update init.lua
    vim.o.signcolumn = 'auto' to remove margins

--- init.lua
+++ init.lua
@@ -129,7 +129,8 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+-- Benoit - changed to 'default', dont need weird margins when theyre not used
+vim.o.signcolumn = 'auto'
 
 -- Decrease update time
 vim.o.updatetime = 250
